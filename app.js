c50baa4a2c68e014e95fe06ac1e6330037e63ef7
smiledgk <111885039+smiledgk@users.noreply.github.com>
2024-01-29 17:48:16 +0100

    app

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const path = require('path');
 const ejsMate = require('ejs-mate');
 const app = express();
 
-
-
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -14,12 +12,6 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.render('calculator.ejs')
 })
-
-app.get('/calculator', (req, res) => {
-    //test github
-    res.render('new.ejs')
-})
-
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000!!')
